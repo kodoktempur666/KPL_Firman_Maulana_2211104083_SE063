f0d2a110f391b1ab42dae4c34d8082183fb59aab
tokna12 <lontopcx12@gmail.com>
2025-03-07 08:26:39 +0700

    pertemuan 1

---
+++ 02_pengenalan_IDE_dan_Pemrograman_NodeJS/variable-local-global.js
@@ -0,0 +1,12 @@
+let globalVarible = "Firman Maulana"
+
+function getMyIdentity() {
+    let localVariable = 2211104083
+
+    console.log(`Nama saya ${globalVarible} nim ${localVariable}`)
+}
+
+console.log(globalVarible)
+// console.log(localVariable)
+
+getMyIdentity()
---
+++ 02_pengenalan_IDE_dan_Pemrograman_NodeJS/variable.js
@@ -0,0 +1,17 @@
+var name = 'Firman Maulana'
+const nim = 2211104083
+
+console.log(name)
+console.log(nim)
+
+console.log('nama saya adalah ' + name + ' dengan NIM ' + nim)
+console.log(`nama saya adalah ${name} dengan NIM ${nim}`)
+
+
+console.log(`Nama saya adalah ${name}
+    nim saya adalah ${nim}`)
+
+
+var name = "ayam"
+
+let fullName = "Firman Maulana"
